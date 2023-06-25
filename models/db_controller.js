f2b3c527014deae18ecc4f4158389ace6afffe37
victor <victoribeojo8@gmail.com>
2023-06-25 18:19:27 +0100

    this is

--- models/db_controller.js
+++ models/db_controller.js
@@ -4,22 +4,22 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 
 
-// var con = mysql.createConnection({
-//     host: '127.0.0.1',//localhost
-//     user: 'root',
-//     password: '',
-//     database: 'hmsystem',
-// });
-
-const con  = mysql.createPool({
-   // connectionLimit : process.env.CONNECTION_LIMIT,
-    host            : process.env.HOST,
-    user            : process.env.USER,
-    password        : '',
-    database        : process.env.DB_NAME,
-   // charset         : process.env.CHARSET,
-    multipleStatements: true
+var con = mysql.createConnection({
+    host: '127.0.0.1',//localhost
+    user: 'root',
+    password: '',
+    database: 'hmsystem',
 });
+
+// const con  = mysql.createPool({
+//    // connectionLimit : process.env.CONNECTION_LIMIT,
+//     host            : process.env.HOST,
+//     user            : process.env.USER,
+//     password        : '',
+//     database        : process.env.DB_NAME,
+//    // charset         : process.env.CHARSET,
+//     multipleStatements: true
+// });
 
 con.connect(function(err){
     if(err){
