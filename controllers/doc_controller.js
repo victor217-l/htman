d6d26b96b2f2e06c8c3a24b15b4780a158d57045
victor <victoribeojo8@gmail.com>
2023-06-29 00:07:01 +0100

    this is

--- controllers/doc_controller.js
+++ controllers/doc_controller.js
@@ -104,17 +104,6 @@ router.get('/add_doctor',   async function(req,res){
     //      })
 }); 
 
-// function authenicateToken(req, res, next)   {
-//     const authHeader = req.headers['authorization']
-//     const token = authHeader && authHeader.split('')[1]
-//     if(token == null ) res.sendStatus(401)
- 
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-//      if(err) res.sendStatus(401)
-//      req.user = user;
-//      next()
-//     })
-// }
 
 router.post('/add_doctor',authenticateToken, upload.single("image"), function(req,res){
 
