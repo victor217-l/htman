ed0bc66222e8f79e86a724c60ff3f0f3cc5ece5d
victor <victoribeojo8@gmail.com>
2023-06-29 19:09:25 +0100

    this is

--- models/db_model.js
+++ models/db_model.js
@@ -154,8 +154,145 @@ var add_doctor  = (first_name, last_name,email,dob, gender,address,phone, image,
 
 }
 
+// module.exports.getDocbyId = function(id, callback){
+//     var query = "select * from doctor where id = ?";
+//     var values = [id];
+//     con.query(query, values, callback);
+//     console.log(query);
+// }
 
 
+var getDocbyId = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection(async (err,connection) => {
+            if(err) throw err;
+            connection.query("select * from doctor where id = ?", [id], async (err,rows) => {
+                connection.release();
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status: true, data:rows})
+                }
+            })
+        })
+    })
+}
+
+// module.exports.editDoc = function(id,first_name, last_name,email,dob, gender,address,phone, image,department,biography,callback){
+//     var query = "update doctor set `first_name` = ? ,`last_name` = ?,`email` = ?,`dob` = ?,`gender` = ?,`address` = ?,`phone` = ?,`image` = ?,`department` = ?,`biography` = ? where id = ?";
+//     var values = [first_name, last_name,email,dob, gender,address,phone, image,department,biography,id];
+//     con.query(query, values,callback); 
+//     console.log(query);
+// }
+
+
+var editDoc = (id,first_name, last_name,email,dob, gender,address,phone, image,department,biography) => {
+    return Promise((resolve,reject) => {
+        pool.getConnection(async (err,connection) => {
+            if(err) throw err
+            connection.query("update doctor set  `firstname` = ?, `lastname`= ?, `email`=?, `dob`=?, `gender`=?, `address` = ?,`phone` = ?,`image` = ?,`department` = ?,`biography` = ? where id = ?", [first_name, last_name,email,dob, gender,address,phone, image,department,biography,id], async (err,rows) =>{
+                connection.release();
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status: true, data:rows})
+                }
+            })
+        })
+    })
+}
+
+
+// module.exports.deleteDoc = function(id,callback){
+//     var query = "delete  from doctor where id = ?";
+//     var values = [id];
+//     con.query(query, values,callback);
+//     console.log(query)
+// }
+
+var deletDoc = (id) => {
+    return Promise((resolve,reject) => {
+        pool.getConnection(async (err,connection) => {
+            if(err) throw err;
+            connection.query("delete * from doctor where id = ?", [id], async (err,rows)=> {
+                connection.release();
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status: true, data: rows})
+                }
+            } )
+        })
+    })
+}
+
+var getAllDoc = () => {
+    return Promise((resolve,reject) => {
+        pool.getConnection(async (err,connection) => {
+            if(err) throw err;
+            connection.query("select * from doctor", async (err,rows) => {
+                connection.release();
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status:true, data:rows})
+                }
+            })
+        })
+    })
+}
+
+
+// module.exports.searchDoc = function(key, callback){
+//     var query = 'select  * from doctor where first_name like "%' + key + '%"';
+//     con.query(query,callback);
+//     console.log(query)
+//  }
+
+ var searchdoc = (key) => {
+    return Promise((resolve, reject) => {
+        pool.getConnection(async(err,connection) => {
+            if(err) throw err;
+            connection.query('select * from doctor where first_name like "%' + key + '%"', async (err,rows) => {
+                connection.release();
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status: true, data: rows})
+
+                }
+
+            });
+        })
+    })
+
+ }
+
+
+//  module.exports.getallappoint =  function(callback){
+//     var query = "select * from appointment";
+//     con.query(query, callback)
+//     console.log(query)
+//  }
+
+
+ var getallappoint = () => {
+    return Promise((resolve, reject) => {
+        pool.getConnection(async(err,connection) => {
+            if(err) throw err;
+            connection.query("select * from appointment", async (err,rows) => {
+                connection.release();
+
+                if(err){
+                    return resolve({status: false})
+                }else{
+                    return resolve({status: true, data:rows})
+                }
+            })
+
+        })
+    })
+ }
 
 
 
@@ -167,6 +304,12 @@ module.exports = {
     check_username_password,
     getAllDoc,
     add_doctor,
+    getDocbyId,
+    editDoc,
+    deletDoc,
+    getAllDoc,
+    searchdoc,
+    getallappoint,
     
 
 }
